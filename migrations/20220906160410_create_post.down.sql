0ce853aaee82054198027738985107d0cfff6ead
aria <aria3ppp@gmail.com>
2022-09-08 19:05:58 +0430

    minor reordering

--- migrations/20220906160410_create_post.down.sql
+++ migrations/20220906160410_create_post.down.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
+DROP FUNCTION IF EXISTS update_posts_trigger_audit;
 DROP TABLE IF EXISTS posts_audit;
 DROP TABLE IF EXISTS posts;
-DROP FUNCTION IF EXISTS update_posts_trigger_audit;
 
 COMMIT;
